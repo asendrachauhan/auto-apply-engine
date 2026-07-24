9025b9d40d9870192489013d1ebd0aa2f4d5f853
Asendra chauahn <127045462+asendrachauhan@users.noreply.github.com>
2026-07-25 00:44:33 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'auto-apply-engine-production.up.railway.app/',  // Replaced by CI/CD with actual Railway URL
+  apiUrl: '/auto-apply-engine-production.up.railway.app',  // Replaced by CI/CD with actual Railway URL
 };
